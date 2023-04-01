08c140f166fcf95add42cd5c6c40394e62405e53
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2023-04-02 00:07:36 +0300

    updated  "version": "1.0.16",

--- src/verifyAutomatedTests.js
+++ src/verifyAutomatedTests.js
@@ -1,5 +1,5 @@
 const {readFile} = require("./helpers/file-helper");
-const {tagReport, duplicateReport} = require("./helpers/constants");
+const {tagReport, duplicateReport, unexistingReport} = require("./helpers/constants");
 
 async function verifyAutomatedTests() {
     const {getUpdatedTestCases, getAddedTestCases} = require("./helpers/tests-helper.js");
@@ -12,6 +12,7 @@ async function verifyAutomatedTests() {
     addedTests.push(...updatedTests)
     verifyTags(addedTests)
     verifyDuplicates(addedTests)
+    verifyUnexisting(addedTests)
 
     if (result.size > 0) {
         console.log(result)
@@ -30,9 +31,18 @@ async function verifyAutomatedTests() {
         let duplicates = readFile(duplicateReport)
         tests.filter(t => duplicates.includes(t))
             .forEach(t => {
-                result.set(t, {"duplicate": duplicates.split("\n").find(l => l.includes(t))})
+                result.set(t, {"duplicate": true})
             })
     }
+
+    function verifyUnexisting(tests) {
+        let unexisting = readFile(unexistingReport)
+        tests.filter(t => unexisting.includes(t))
+            .forEach(t => {
+                result.set(t, {"unexist": true})
+            })
+    }
+
 }
 
 module.exports = {verifyAutomatedTests}
