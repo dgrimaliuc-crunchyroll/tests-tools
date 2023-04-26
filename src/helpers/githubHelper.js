2f46e900359252c6b12961e2dc6afddb9b24a4ef
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2023-04-26 10:50:00 +0300

    Added comment in PR for test-check job updated "version": "1.0.24"

--- src/helpers/githubHelper.js
+++ src/helpers/githubHelper.js
@@ -12,4 +12,41 @@ async function getCurrentPRInfo() {
 
 }
 
-module.exports = {getCurrentPRInfo}
+async function notifyFailures(body) {
+    const github = require('@actions/github').getOctokit(process.env.GITHUB_TOKEN);
+    const context = {
+        repo: process.env.CIRCLE_PROJECT_REPONAME,
+        owner: process.env.CIRCLE_PROJECT_USERNAME,
+        issue_number: process.env.CIRCLE_PULL_REQUEST?.match(/\d+/)?.[0],
+    };
+    const comments = await github.rest.issues.listComments(context);
+    const commentId = `<!--< Comment from ${process.env.CIRCLE_JOB} job >-->`;
+    const comment = comments.data.find((comment) => comment.body.indexOf(commentId) !== -1);
+    const summary = `${commentId}\n${body}`
+
+    if (body) {
+        if (comment) {
+            console.log('Update');
+            await github.rest.issues.updateComment({
+                ...context,
+                comment_id: comment.id,
+                body: summary
+            });
+        } else {
+            console.log('Create');
+            await github.rest.issues.createComment({
+                ...context,
+                body: summary
+            });
+        }
+    } else if (comment) {
+        console.log('Delete');
+        await github.rest.issues.deleteComment({
+            ...context,
+            comment_id: comment.id
+        });
+    }
+}
+
+
+module.exports = {getCurrentPRInfo, notifyFailures}
